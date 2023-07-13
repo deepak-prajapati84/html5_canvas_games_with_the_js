7b79ad8c25faebb27fe293a43afc96a6e6d2f0e9
Deepak kumar <dkp0043@gmail.com>
2023-07-13 09:13:23 +0530

    Classes create

---
+++ js/script2.js
@@ -0,0 +1,53 @@
+// window.onload=function myFunction(){
+//     console.log("hello Deepak");
+// }
+
+class MyGame{
+    //Propaties:value
+
+
+    //constructor
+
+
+    //method
+    start(){
+        console.log("heello skna");
+    }
+
+}
+
+let myGame=new MyGame();
+
+
+window.addEventListener('load',function(){
+    console.log("Hello Prajapati");
+
+    myGame.start();
+})
+
+
+
+
+/*class MySudent{
+    //p:v
+    nm1='';
+    nm2='';
+    n3m='';
+
+    //constructor
+    constructor(n1,n2,n3){
+        this.nm1=n1;
+        this.nm2=n2;
+        this.n3m=n3;
+    }
+
+    //methods/function
+    getMyFunction(){
+        console.log("Deepak "+this.n1+this.n2);
+    }
+}
+
+let sut=new MySudent("Kumar",`Suraj`,'pankaj');
+//classObject.method
+sut.getMyFunction();
+*/
